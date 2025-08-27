0862a5e28016fb918c32724d6e77195d245c3d1d
Stan Abada <abadastan22@gmail.com>
2025-08-27 07:10:35 -0400

    building chat bolt

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
+import z from 'zod';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -14,6 +18,32 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
+});
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Prompt is too long (Max is 1000 characters'),
+   conversationId: z.string().uuid(),
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format);
+      return;
+   }
+
+   try {
+      const { prompt, conversationId } = req.body;
+      const response = await chatService.sendMessage(prompt, conversationId);
+
+      res.json({ message: response.message });
+   } catch (error) {
+      res.status(500).json({ error: 'Failed to generate a response.' });
+   }
 });
 
 app.listen(port, () => {
