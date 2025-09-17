baaf7533567ccb080f493deb92df8018b05afda1
Stan Abada <abadastan22@gmail.com>
2025-09-16 21:35:13 -0400

    storing and extracting llm logic and prompt

---
+++ packages/server/repositories/review.repository.ts
@@ -0,0 +1,38 @@
+import dayjs from 'dayjs';
+import { PrismaClient, type Review } from '../generated/prisma';
+
+const prisma = new PrismaClient();
+export const reviewRepository = {
+   async getReviews(productId: number, limit?: number): Promise<Review[]> {
+      // SELECT * FROM reviews where productId = @productId ORDER BY createdAt DESC
+      return prisma.review.findMany({
+         where: { productId },
+         orderBy: { createdAt: 'desc' },
+         take: limit,
+      });
+   },
+
+   storeReviewSummary(productId: number, summary: string) {
+      const now = new Date();
+      const expiresAt = dayjs().add(7, 'days').toDate();
+
+      const data = {
+         content: summary,
+         expiresAt,
+         generatedAt: now,
+         productId,
+      };
+
+      return prisma.summary.upsert({
+         where: { productId },
+         create: data,
+         update: data,
+      });
+   },
+
+   getReviewSummary(productId: number) {
+      return prisma.summary.findUnique({
+         where: { productId },
+      });
+   },
+};
